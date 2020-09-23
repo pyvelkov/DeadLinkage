10819d08f7894dbede4ef1dd779b11841d51fb98
chrispinkney <hey@chrispinkney.com>
2020-09-23 19:28:45 -0400

    removed comments

--- src/DeadLinkTest.java
+++ src/DeadLinkTest.java
@@ -11,14 +11,10 @@ import java.util.List;
 
 
 public class DeadLinkTest {
-
-    //colors
-    //text
     public static final String RED = "\033[0;31m";
     public static final String GREEN = "\033[0;32m";
     public static final String WHITE = "\033[0;37m";
     public static final String RESET = "\033[0m";
-    //bgd
     public static final String RED_BACKGROUND = "\033[41m";
     public static final String GREEN_BACKGROUND = "\033[42m";
 
@@ -29,26 +25,21 @@ public class DeadLinkTest {
         if (args.length == 0){
             driver.quit();
             helpMessage();
-            //System.exit(0);
         }
         else if (args[0].equals("-v")){
             driver.quit();
             versionMessage();
-            //System.exit(0);
         }
         else if(args[0].equals("-f") && args[1].endsWith(".html")){
-            /*using relative path============================================*/
             Path pathFile = Paths.get(args[1]);
             driver.get(pathFile.toUri().toString());
         }
         else if(args[0].equals("-u") && args[1].contains("http")){
-            /*using website=================================================*/
             driver.get(args[1]);
         }
         else{
             driver.quit();
             helpMessage();
-            //System.exit(0);
         }
 
         driver.manage().window().maximize();
@@ -108,23 +99,14 @@ public class DeadLinkTest {
         try
         {
             URL url = new URL(linkUrl);
-
             HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
-
             httpURLConnect.setRequestMethod("HEAD");
-
-            //use this so I don't get my ip blacklisted for scraping too much
-            //httpURLConnect.setConnectTimeout(1000);
-
             httpURLConnect.connect();
 
             if(httpURLConnect.getResponseCode() == 200)
             {
                 System.out.println(GREEN + linkUrl + "   ---->   " + "[" + WHITE + GREEN_BACKGROUND + HttpURLConnection.HTTP_OK + RESET + GREEN + "] - " + httpURLConnect.getResponseMessage() + RESET);
             }
-            /*else if(httpURLConnect.getResponseCode()>=300 && httpURLConnect.getResponseCode()<=399){
-                System.out.println(RED + linkUrl + "   ---->   " + WHITE + RED_BACKGROUND + httpURLConnect.getResponseMessage() +  " - " + HttpURLConnection.HTTP_MOVED_PERM + RESET);
-            }*/
             else if(httpURLConnect.getResponseCode() >= 400 && httpURLConnect.getResponseCode() <= 499)
             {
                 System.out.println(RED + linkUrl + "   ---->   " + "[" + WHITE + RED_BACKGROUND + HttpURLConnection.HTTP_NOT_FOUND + RESET + RED + "] - " + httpURLConnect.getResponseMessage() + RESET);
