bda13e7572eee036659aea86771c547f0863b6ae
pyvelkov <pyvelkov@users.noreply.github.com>
2020-11-17 15:21:37 -0500

    test start in TestingBranch

---
+++ Test/java/DeadLinkage/LoadIgnoreFileTest.java
@@ -0,0 +1,13 @@
+package DeadLinkage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoadIgnoreFileTest {
+
+    @Test
+    public void loadFileIgnore() {
+        
+    }
+}
